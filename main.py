ed48505bfed4c8ca3728eb260455199f9dda6e52
Emanuel Tesfa <emanueltesfa0@gmail.com>
2022-09-12 21:41:05 -0700

    Population Created

--- main.py
+++ main.py
@@ -1,11 +1,35 @@
+from population import Population
+import random 
+
 def create_init_pop(size, cities):
     inital_population = [] 
-    # your code goes here
+    keys = range(5)
+    pop_setup = [None] * 5
+    pop_dict = {}
 
+    population = set()
+    for k in range(100):
+        for i in range(5):  # setting random val to value var
+            pop_setup[i] = random.random()
+        for i in keys:
+            pop_dict[i] = pop_setup[i] # create dict
+
+        temp = (sorted(pop_dict.items(), key=lambda kv: kv[1])) # sort
+
+        temp = dict(temp)
+        temp = list(temp.keys())
+        population.add(temp)  # create list of dict
+        
+    print(population)
+    print(type(population))
 
     return inital_population
 
-def create_mapping_pool(population, rankList): 
+def calc_fitness ():
+    score = 0 
+    return score
+
+def create_mating_pool(population, rankList): 
     matingPool  = [] 
     # your code goes here 
 
@@ -17,21 +41,20 @@ def crossover(parent1, parent2, start_index, end_index):
 
     return child 
 
-def search_best_paths():
-
-    return None
-
 def agent(coordinate_list):
+
     return 0
 
 if __name__=="__main__":
     #PASS IN INPUT.TXT
     
     input = []
-    f = open("input.txt", 'r')
+    f = open("Test_Cases/input.txt", 'r')
     for x in f: 
         x = x.strip()
         input.append(x)
+    input = input[1:]
+    create_init_pop(size = len(input), cities=input)
 
-    agent(input)
+    #agent(input)
 
---
+++ population.py
@@ -0,0 +1,15 @@
+def Population():
+    def __init__ (self, size, parents, score, best):
+        self.size = size 
+        self.parents = parents
+        self.score = score 
+        self.best = best
+
+    def create_init_pop(size, cities):
+        inital_population = [] 
+        
+        # create array of random variables with size of n + 1
+        # set first and last index to one start point 
+        
+
+        return inital_population
