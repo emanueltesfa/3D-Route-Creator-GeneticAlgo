c654c1dc61684582fd95eda0ad5f1a9538de3d25
Emanuel Tesfa <emanueltesfa0@gmail.com>
2022-09-15 21:45:51 -0700

    New pop working/Calc fitness WIP

--- main.py
+++ main.py
@@ -1,67 +1,99 @@
-from population import Population
-import random 
+from mimetypes import init
+import random
+import numpy as np
+from main import *
 
-def create_init_pop(size, cities):
-    inital_population = [] 
-    keys = range(5)
-    pop_setup = [None] * 5
-    pop_dict = {}
+k = 100  # population size
 
-    population = []
-    for k in range(100):
-        for i in range(5):  # setting random val to value var
-            pop_setup[i] = random.random()
-        for i in keys:
-            pop_dict[i] = pop_setup[i] # create dict
 
-        temp = (sorted(pop_dict.items(), key=lambda kv: kv[1])) # sort
+class Path():
+    def __init__(self, path, total_fitness):
+        self.path = path
+        self.total_fitness = total_fitness
 
-        temp = dict(temp)
-        temp = list(temp.keys())
-        population.append(temp)  # create list of dict
-        
-    # print(population)
-    # print(type(population))
+    def calc_fitness(self, inital_population):
+        print("reached calc fitness")
 
-    return population
+        for i in range(k - 1):
+            temp1 = objs[i].path
+            temp2 = objs[i + 1].path[1]
 
-def calc_fitness (inital_population):
-    score = 0 
+            #print(temp1)
+            #x = [int(i) for i in temp1]
+            #print(x)
+            #temp1 = temp1.split(" ")
+            # print(temp1)
 
-    for subarr in inital_population:
-        subarr
-    
-    return score
 
-def create_mating_pool(population, rank_list): 
-    matingPool  = [] 
-    # your code goes here 
+objs = [Path(None, None) for i in range(100)]  # global list of objects
 
+
+class Population():
+    def __init__(self, size, parents, best):
+        self.size = size
+        self.parents = parents
+        self.best = best
+
+
+def create_mating_pool(population, rank_list):
+    matingPool = []
+    # your code goes here
     return matingPool
+
 
 def crossover(parent1, parent2, start_index, end_index):
     child = []
     # your code goes here
 
-    return child 
+    return child
+    #path1.fitness(path2)
+
+
+
+def create_init_pop(size, cities):
+    for i in range(k):
+        np.random.shuffle(cities)
+        #objs[i].path = cities 
+        
+        setattr(objs[i], 'path', list(cities))
+        #print(objs[i].path)
+
+    #print(objs[0].path)
+    #print(objs[1].path)
+
+
+# return score
 
 def agent(size, cities):
-    num_of_loc = size 
-    coordinates = cities 
-    inital_pop = create_init_pop(size = num_of_loc, cities = coordinates)
-    print(inital_pop)
-    calc_fitness(inital_population=inital_pop)
+    num_of_loc = size
+    coordinates = cities
+    init_pop = create_init_pop(size = num_of_loc, cities = coordinates)
+
+    path = Path(init_pop, 0 )
+    print(objs[0].path)
+    print(objs[99].path)
+    path.calc_fitness(init_pop)
+
     return 0
 
-if __name__=="__main__":
-    #PASS IN INPUT.TXT
-    
+
+if __name__ == "__main__":
+    # PASS IN INPUT.TXT
+
     input = []
+    # input = np.zeros(0)
     f = open("Test_Cases/input.txt", 'r')
-    for x in f: 
+    for x in f:
         x = x.strip()
+        #temp = []
+        #temp = x.split(" ")
+
+        #x = [int(i) for i in temp]
+        #print(temp)
         input.append(x)
     input = input[1:]
-    agent(size = len(input), cities=input)
+    input = np.asarray(input)
+    #print((input))
+    agent(size=len(input), cities=input)
 
-    #agent(input)
+    # agent(input)
