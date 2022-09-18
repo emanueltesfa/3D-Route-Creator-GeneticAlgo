a1503b38f8704883331c6ae172383c993edf4ade
Emanuel Tesfa <61919344+emanueltesfa@users.noreply.github.com>
2022-09-17 20:11:50 -0700

    Reformatted main parse to int

--- main.py
+++ main.py
@@ -17,48 +17,23 @@ class Path():
         final = []
         dist_arr = []
 
-        # print(objs[0].path)
-        for i in range(k):
-            temp = objs[i].path
-            mid_store = []
-            for j in range(len(temp)):
-                mytuple = list(map(int, temp[j].split(' ')))
-                mid_store.append(mytuple)
-            final.append(mid_store)
-        # print(final[0][0][0])
+        # (final[0][0][0])
         total_cost = 0.00
         for i in range(k):
-            # print(len(point1))
             dist = 0.0000000
-
-            #print(f"set {i} of 100")
             for j in range(len(final[i])-1):
                 point1 = np.asarray(final[i][j])
                 point2 = np.asarray(final[i][j+1])
-                #print("Point1 is ", point1, " \n Point2 is ", point2)
                 dist += np.linalg.norm(point1 - point2)
-                #print("distance is ", dist)
-                #total_cost += (1/dist)
-            dist_arr.append(dist)
-            setattr(objs[i], "path_cost", (dist))
-            total_cost += 1 / dist
-        print("total cost of generation is: ", total_cost)
-        # total_cost = 1 / total_cost
-        delete_count = 0.000
-        delete_fitness = 0.00
-        for i in range(k):
 
+            dist_arr.append(dist)
+            objs[i].path_cost = dist
+            # setattr(objs[i], "path_cost", dist)
+            total_cost += 1 / dist
+
+        for i in range(k):
             fitness = (1 / objs[i].path_cost) / total_cost
             setattr(objs[i], "fitness", fitness)
-
-            delete_count += objs[i].path_cost
-            delete_fitness += fitness
-            #print(total_cost, objs[i].path_cost, fitness)
-        # print(delete_fitness)
-        # print(dist_arr)
-        #
-        #x = [int(i) for i in temp1]
-        #temp1 = temp1.split(" ")
 
 
 objs = [Path(None, None, None) for i in range(100)]  # global list of objects
@@ -72,10 +47,7 @@ class Population():
 
 
 def create_mating_pool(population, rank_list):
-    matingPool = []
-
     max = sum([c.fitness for c in population])
-    print("Max is ", max)
     selection_probs = [c.fitness/max for c in population]
     return population[np.random.choice(len(population), p=selection_probs)]
 
@@ -83,8 +55,9 @@ def create_mating_pool(population, rank_list):
 
 
 def crossover(parent1, parent2, start_index, end_index):
+    # parent1 = [120 199 34], [137 199 93], [199 173 30],[144 39 130], [175 53 76], [153 196 97], [173 101 186]
     child = []
-    # your code goes here
+
     return child
 
 
@@ -104,15 +77,8 @@ def agent(size, cities):
     path.calc_fitness(init_pop)
     objs.sort(key=lambda x: x.path_cost)
     idk = create_mating_pool(objs, None)
-    print("idk is: ", idk.path, "\n Path cost is: ",
-          idk.fitness, "\n Fitness is: ", idk.fitness)
-    dist = 0.00
-    for j in range(len(idk)-1):
-        point1 = np.asarray(idk[j])
-        point2 = np.asarray(idk[j+1])
-        #print("Point1 is ", point1, " \n Point2 is ", point2)
-        dist += np.linalg.norm(point1 - point2)
-    print(dist)
+    # print("idk is: ", idk.path, "\n Path cost is: ",
+    # idk.path_cost, "\n Fitness is: ", idk.fitness)
 
 
 def parse_input():
@@ -127,15 +93,14 @@ if __name__ == "__main__":
     f = open("Test_Cases/input.txt", 'r')
     for x in f:
         x = x.strip()
-        #temp = []
-        #temp = x.split(" ")
-
-        #x = [int(i) for i in temp]
-        # print(temp)
         input.append(x)
     input = input[1:]
     input = np.asarray(input)
-    # print((input))
-    agent(size=len(input), cities=input)
+
+    final = []
+    for j in range(len(input)):
+        mytuple = list(map(int, input[j].split(' ')))
+        final.append(mytuple)
+    agent(size=len(final), cities=final)
 
     # agent(input)
